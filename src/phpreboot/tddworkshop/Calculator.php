9a6f32464115fef51334be73d4f25b02e08656ee
Manoj <mailto:manojatakare@djalexander.in>
2018-06-22 13:18:42 +0530

    Task 2

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -13,13 +13,25 @@ class Calculator
 {
     public function add($numbers)
     {
+
         if (empty($numbers)) {
             return 0;
         }
+        if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+
+        $numbers = str_replace("n", ",", $numbers);
+
 
         $numbersArray = explode(",", $numbers);
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
 
         return array_sum($numbersArray);
     }
 
 }
+
