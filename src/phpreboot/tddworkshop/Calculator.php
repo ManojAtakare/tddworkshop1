62cee375d6b14914932da3f8884ce495f14f119c
Manoj <mailto:manojatakare@djalexander.in>
2018-06-25 11:33:44 +0530

    Test1

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -21,9 +21,6 @@ class Calculator
             throw new \InvalidArgumentException('Parameters must be a string');
         }
 
-        $numbers = str_replace("n", ",", $numbers);
-
-
         $numbersArray = explode(",", $numbers);
 
         if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
@@ -31,6 +28,26 @@ class Calculator
         }
 
         return array_sum($numbersArray);
+    }
+
+
+    public function mul($numbers)
+    {
+
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+
+        $numbersArray = explode(",", $numbers);
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
+
+        return array_product($numbersArray);
     }
 
 }
