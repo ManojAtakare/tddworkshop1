0bdd165edea8cfe2a3f047f229a5d3e3c78f9ea5
Manoj Atakare <manojatakare276@gmail.com>
2018-06-25 12:16:01 +0530

    Update Calculator.php

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -37,13 +37,45 @@ class Calculator
             throw new \InvalidArgumentException("Negative numbers (".$negativeNo.") not allowed.");
         }
 
-
-
         if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
             throw new \InvalidArgumentException('Parameters string must contain numbers');
         }
 
         return array_sum($numbersArray);
+    }
+
+
+    public function mul($numbers)
+    {
+
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+        $numbers = stripslashes($numbers);
+        $numbers = explode(",", $numbers);
+
+        $negative = array_filter($numbers, function ($v) {
+            return $v < 0;
+        });
+
+        $numbersArray = array_filter($numbers, function ($v) {
+            return $v > 0;
+        });
+
+
+        if(count($negative) > 0){
+            $negativeNo = implode(',',$negative);
+            throw new \InvalidArgumentException("Negative numbers (".$negativeNo.") not allowed.");
+        }
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
+
+        return array_product($numbersArray);
     }
 
 }
