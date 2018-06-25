38c4c27a0afe7d5b26d01ede26855925d3752569
Manoj Atakare <manojatakare276@gmail.com>
2018-06-25 11:49:00 +0530

    Update Calculator.php

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -21,7 +21,8 @@ class Calculator
             throw new \InvalidArgumentException('Parameters must be a string');
         }
 
-        $numbers = str_replace("n", ",", $numbers);
+
+
 
         $numbersArray = explode(",", $numbers);
 
@@ -30,6 +31,29 @@ class Calculator
         }
 
         return array_sum($numbersArray);
+    }
+
+
+    public function mul($numbers)
+    {
+
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+
+
+
+
+        $numbersArray = explode(",", $numbers);
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
+
+        return array_product($numbersArray);
     }
 
 }
