4fb8c276f25b3c63ecdd24b5cf106eace21942db
Manoj <mailto:manojatakare@djalexander.in>
2018-06-25 10:32:08 +0530

    test 5

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -13,20 +13,31 @@ class Calculator
 {
     public function add($numbers)
     {
-        
+
         if (empty($numbers)) {
             return 0;
         }
         if (!is_string($numbers)) {
             throw new \InvalidArgumentException('Parameters must be a string');
         }
-
-        $regex = ['@','$','!','%',"^",'&','*','(',')','-','+','_',';','{','}','[',']'];
         $numbers = stripslashes($numbers);
-        $numbers = str_replace($regex, ",", $numbers);
-        $numbers = trim($numbers,',');
+        $numbers = explode(",", $numbers);
 
-        $numbersArray = explode(",", $numbers);
+        $negative = array_filter($numbers, function ($v) {
+            return $v < 0;
+        });
+
+        $numbersArray = array_filter($numbers, function ($v) {
+            return $v > 0;
+        });
+
+
+        if(count($negative) > 0){
+           // $negativeNo = implode(',',$negative);
+            throw new \InvalidArgumentException("Negative numbers not allowed.");
+        }
+
+
 
         if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
             throw new \InvalidArgumentException('Parameters string must contain numbers');
