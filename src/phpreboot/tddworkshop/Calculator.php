572840e2dad7398c2ead1dae658d2103ac97305b
Manoj <mailto:manojatakare@djalexander.in>
2018-06-25 11:07:20 +0530

    Test7

--- src/phpreboot/tddworkshop/Calculator.php
+++ src/phpreboot/tddworkshop/Calculator.php
@@ -36,5 +36,31 @@ class Calculator
         return array_sum($numbersArray);
     }
 
+
+    public function mul($numbers)
+    {
+
+        if (empty($numbers)) {
+            return 0;
+        }
+        if (!is_string($numbers)) {
+            throw new \InvalidArgumentException('Parameters must be a string');
+        }
+        $numbers = stripslashes($numbers);
+        $numbers = explode(",", $numbers);
+
+
+        $numbersArray = array_filter($numbers, function ($v) {
+            return $v < 1000;
+        });
+
+
+        if (array_filter($numbersArray, 'is_numeric') !== $numbersArray) {
+            throw new \InvalidArgumentException('Parameters string must contain numbers');
+        }
+
+        return array_product($numbersArray);
+    }
+
 }
 
